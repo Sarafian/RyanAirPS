853179e57d88dc34869f1b0e699e4acc261cba3e
a.sarafian <dev1978@outlook.com>
2016-06-11 19:55:53 +0200

    Fixed publish information

--- Publish/PreparePSD1.ps1
+++ Publish/PreparePSD1.ps1
@@ -1,6 +1,6 @@
 ﻿& "$PSScriptRoot\..\ISEScripts\Reset-Module.ps1"
 
-$exportedNames=Get-Command -Module MarkDownPS | Select-Object -ExcludeProperty Name
+$exportedNames=Get-Command -Module RyanAirPS | Select-Object -ExcludeProperty Name
 
 . "$PSScriptRoot\Version.ps1"
 $semVersion=Get-Version
@@ -21,23 +21,22 @@ foreach($module in $modules)
     $psd1Name=$name+".psd1"
     $psd1Path=Join-Path $module.FullName $psd1Name
 
-    $guid="30e93b28-36d9-42fd-ace1-14b8eda0ded1"
+    $guid="035a9a53-9bd7-4634-8349-d7e72bedd241"
     $hash=@{
-        "Author"=$author;
-        "Copyright"=$cop;
-        "RootModule"=$psm1Name;
-        "Description"=$description;
-        "Guid"=$guid;
-        "ModuleVersion"=$semVersion;
-        "Path"=$psd1Path;
-        "Tags"=@('Markdown', 'Tools');
-        "LicenseUri"='https://github.com/Sarafian/RyanAirPS/blob/master/LICENSE';
-        #"ProjectUri"= 'http://sarafian.github.io/RyanAirPS/';
-        "IconUri" ='https://github.com/Sarafian/RyanAirPS/blob/master/RyanAir.jpg';
-        "ReleaseNotes"= 'https://github.com/Sarafian/MarkdownPS/blob/master/CHANGELOG.md';
-        "CmdletsToExport" = $exportedNames;
-        "FunctionsToExport" = $exportedNames;
-        "PowerShellHostVersion"="4.0"
+        "Author"=$author
+        "Copyright"=$copyright
+        "RootModule"=$psm1Name
+        "Description"=$description
+        "Guid"=$guid
+        "ModuleVersion"=$semVersion
+        "Path"=$psd1Path
+        "Tags"=@('RyanAirPS', 'Tools')
+        "LicenseUri"='https://github.com/Sarafian/RyanAirPS/blob/master/LICENSE'
+        "ProjectUri"= 'http://github.com/Sarafian/RyanAirPS/'
+        "IconUri" ='https://github.com/Sarafian/RyanAirPS/blob/master/RyanAir.jpg'
+        "ReleaseNotes"= 'https://github.com/Sarafian/RyanAirPS/blob/master/CHANGELOG.md'
+        "CmdletsToExport" = $exportedNames
+        "FunctionsToExport" = $exportedNames
     }
 
     New-ModuleManifest  @hash 
